a713c881cdc0097104ed476fff3953ff5ff22252
Michael Wender <michael@michaelwender.com>
2019-07-31 14:38:45 -0400

    First commit

---
+++ shufflejs-post-filter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     ShuffleJS Post Filter
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Displays an archive of posts along with filters using Shuffle.js
+ * Author:          TheWebist
+ * Author URI:      https://mwender.com
+ * Text Domain:     shufflejs-post-filter
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Shufflejs_Post_Filter
+ */
+
+// Your code starts here.
