cd0d8a9fb6685c55dbfeb32e35f26a320d27fb0c
Michael Wender <michael@michaelwender.com>
2020-06-17 09:36:27 -0400

    Documenting 1.0.0

--- shufflejs-post-filter.php
+++ shufflejs-post-filter.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     shufflejs-post-filter
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  *
  * @package         Shufflejs_Post_Filter
  */
