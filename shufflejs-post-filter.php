f58e968dec63bc9cfaa0f8c7037e2ceb3a3cf5af
Michael Wender <michael@michaelwender.com>
2020-11-11 14:31:18 -0500

    Setting `All` to selected on init

--- shufflejs-post-filter.php
+++ shufflejs-post-filter.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     shufflejs-post-filter
  * Domain Path:     /languages
- * Version:         1.2.7
+ * Version:         1.3.0
  *
  * @package         Shufflejs_Post_Filter
  */
