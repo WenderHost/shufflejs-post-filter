7858b5b59b014598d424826cff2b489a0cc55b1a
Michael Wender <michael@michaelwender.com>
2021-01-20 13:33:56 -0500

    Bumping version number

--- shufflejs-post-filter.php
+++ shufflejs-post-filter.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     shufflejs-post-filter
  * Domain Path:     /languages
- * Version:         1.5.0
+ * Version:         1.5.1
  *
  * @package         Shufflejs_Post_Filter
  */
