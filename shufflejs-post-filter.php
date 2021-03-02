e7ac1fc94ec15b412f0bcf4e5883d808f9982220
Michael Wender <michael@michaelwender.com>
2021-03-02 08:07:47 -0500

    Adding Spanish translation

--- shufflejs-post-filter.php
+++ shufflejs-post-filter.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     shufflejs-post-filter
  * Domain Path:     /languages
- * Version:         1.8.2
+ * Version:         1.9.0
  *
  * @package         Shufflejs_Post_Filter
  */
